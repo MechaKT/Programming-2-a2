acd7f80fc1e80c4ddbeb64e6aab99abdf768b455
Steven Ouyang <stevenouy@hotmail.com>
2020-12-11 11:09:01 -0800

    Commiting mapit and lesson 6

---
+++ webscraping/mapit.py
@@ -0,0 +1,16 @@
+# mapit.py - tool to search physical addresses
+# usage -- python mapit.py 6600 Williams Road Richmond
+#    output: should take us to the address in GMaps
+
+import webbrowser
+import sys
+
+address = " ".join(sys.argv[1:])
+# join(list) -> string
+# Takes a list and gives it a string
+
+# TODO: open the browser at a GMaps page with the arguments
+prefix = "https://google.com/maps/place/"
+webbrowser.open(prefix + address)
+
+# TODO: add feature to grab address from clipboard
